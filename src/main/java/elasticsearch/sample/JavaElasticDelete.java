4fcbaecb57dbf22cb6dfd739a418db2078959bd1
tomotakashimizu <tomotaka.developer@gmail.com>
2021-09-04 06:30:04 +0900

    [update] 特定の index id のデータを削除

--- src/main/java/elasticsearch/sample/JavaElasticDelete.java
+++ src/main/java/elasticsearch/sample/JavaElasticDelete.java
@@ -3,6 +3,9 @@ package elasticsearch.sample;
 import java.io.IOException;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 
@@ -12,5 +15,11 @@ public class JavaElasticDelete {
         // Elasticsearch に接続
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+        // 特定の index id のデータを削除
+        DeleteRequest deleteRequest = new DeleteRequest("employeeindex", "002");
+        DeleteResponse deleteResponse = client.delete(deleteRequest, RequestOptions.DEFAULT);
+        // 削除した id を確認
+        System.out.println("response id: " + deleteResponse.getId());
     }
 }
