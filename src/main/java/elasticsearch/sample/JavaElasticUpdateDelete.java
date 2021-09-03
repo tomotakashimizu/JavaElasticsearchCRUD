ebd53f448ae6713766028b8ab19222a4d1521f30
tomotakashimizu <tomotaka.developer@gmail.com>
2021-09-04 04:42:51 +0900

    [update] update way2 データ更新（元からデータがあるidの場合は一新される）
    変更前
    {
        "_index" : "employeeindex",
        "_type" : "_doc",
        "_id" : "001",
        "_score" : 1.0,
        "_source" : {
            "name" : "Elon Musk",
            "age" : 49,
            "company" : "Tesla",
            "designation" : "CEO",
            "department" : "Global",
            "experienceInYears" : 28
        }
    },
    {
        "_index" : "employeeindex",
        "_type" : "_doc",
        "_id" : "002",
        "_score" : 1.0,
        "_source" : {
            "name" : "Sundar Pichai",
            "age" : 47,
            "company" : "Google",
            "designation" : "CEO",
            "department" : "Bussiness",
            "experienceInYears" : 25,
            "firstname" : "Sundar",
            "sector" : "IT",
            "lastname" : "Pichai"
        }
    }
    
    変更後
    {
      "_index" : "employeeindex",
      "_type" : "_doc",
      "_id" : "001",
      "_score" : 1.0,
      "_source" : {
        "company" : "SpaceX"
      }
    },
    {
      "_index" : "employeeindex",
      "_type" : "_doc",
      "_id" : "002",
      "_score" : 1.0,
      "_source" : {
        "firstname" : "Sundar",
        "company" : "Google",
        "sector" : "IT",
        "lastname" : "Pichai"
      }
    }

--- src/main/java/elasticsearch/sample/JavaElasticUpdateDelete.java
+++ src/main/java/elasticsearch/sample/JavaElasticUpdateDelete.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -18,7 +18,7 @@ public class JavaElasticUpdateDelete {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
-        // // Elasticsearch の String 型でデータ更新
+        // // Elasticsearch の String 型でデータ更新, 追加
         // UpdateRequest updateRequest = new UpdateRequest("employeeindex", "002");
         // updateRequest.doc("department", "Bussiness");
         // // 更新されたか id の確認
@@ -26,15 +26,39 @@ public class JavaElasticUpdateDelete {
         // RequestOptions.DEFAULT);
         // System.out.println("updated response id: " + updateResponse.getId());
 
-        // Map を作成
+        // // Map を作成
+        // Map<String, Object> updateMap = new HashMap<String, Object>();
+        // updateMap.put("firstname", "Sundar");
+        // updateMap.put("lastname", "Pichai");
+        // updateMap.put("company", "Google");
+        // updateMap.put("sector", "IT");
+        // // Elasticsearch を Map 型でデータ更新, 追加
+        // UpdateRequest request = new UpdateRequest("employeeindex",
+        // "002").doc(updateMap);
+        // UpdateResponse updateResponse = client.update(request,
+        // RequestOptions.DEFAULT);
+        // System.out.println("updated response id: " + updateResponse.getId());
+
+        // update way2 Elasticsearch の String 型でデータ更新（元からデータがあるidの場合は一新される）
+        IndexRequest request = new IndexRequest("employeeindex");
+        request.id("001");
+        request.source("company", "SpaceX");
+        IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
+        System.out.println("response id: " + indexResponse.getId());
+        System.out.println(indexResponse.getResult().name());
+
+        // update way2
+        // Elasticsearch を Map 型でデータ更新（元からデータがあるidの場合は一新される）
         Map<String, Object> updateMap = new HashMap<String, Object>();
         updateMap.put("firstname", "Sundar");
         updateMap.put("lastname", "Pichai");
         updateMap.put("company", "Google");
         updateMap.put("sector", "IT");
-        // Elasticsearch を Map 型でデータ更新, 追加
-        UpdateRequest request = new UpdateRequest("employeeindex", "002").doc(updateMap);
-        UpdateResponse updateResponse = client.update(request, RequestOptions.DEFAULT);
-        System.out.println("updated response id: " + updateResponse.getId());
+        IndexRequest request2 = new IndexRequest("employeeindex");
+        request2.id("002");
+        request2.source(updateMap);
+        IndexResponse indexResponseUpdate = client.index(request2, RequestOptions.DEFAULT);
+        System.out.println("response id: " + indexResponseUpdate.getId());
+        System.out.println(indexResponse.getResult().name());
     }
 }
