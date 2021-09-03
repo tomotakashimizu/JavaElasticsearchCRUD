bb83b8437eee21031647582668b1cc1ecf17355a
tomotakashimizu <tomotaka.developer@gmail.com>
2021-09-04 06:37:00 +0900

    [update] Elasticsearch の特定の index ごと削除

--- src/main/java/elasticsearch/sample/JavaElasticDelete.java
+++ src/main/java/elasticsearch/sample/JavaElasticDelete.java
@@ -3,8 +3,7 @@ package elasticsearch.sample;
 import java.io.IOException;
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -16,10 +15,17 @@ public class JavaElasticDelete {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
-        // 特定の index id のデータを削除
-        DeleteRequest deleteRequest = new DeleteRequest("employeeindex", "002");
-        DeleteResponse deleteResponse = client.delete(deleteRequest, RequestOptions.DEFAULT);
-        // 削除した id を確認
-        System.out.println("response id: " + deleteResponse.getId());
+        // // 特定の index id のデータを削除
+        // DeleteRequest deleteRequest = new DeleteRequest("employeeindex", "002");
+        // DeleteResponse deleteResponse = client.delete(deleteRequest,
+        // RequestOptions.DEFAULT);
+        // // 削除した id を確認
+        // System.out.println("response id: " + deleteResponse.getId());
+
+        // Delete the index
+        // 特定の index ごと削除
+        DeleteIndexRequest request = new DeleteIndexRequest("employeeindex");
+        client.indices().delete(request, RequestOptions.DEFAULT);
+        System.out.println("Delete Done ");
     }
 }
