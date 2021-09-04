539cd2422b6c6eb7fc0c419f2ba331d0cf058540
tomotakashimizu <tomotaka.developer@gmail.com>
2021-09-05 03:53:54 +0900

    [add] 特定の index id のデータを削除する class を作成
    JavaElasticDelete.java の処理を分離して個々に実行できるようにするため

---
+++ src/main/java/elasticsearch/sample/DeleteIndexIdData.java
@@ -0,0 +1,27 @@
+package elasticsearch.sample;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+// 特定の index id のデータを削除
+public class DeleteIndexIdData {
+
+    public static void main(String[] args) throws IOException {
+        // Elasticsearch に接続
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+        // 特定の index id のデータを削除
+        DeleteRequest deleteRequest = new DeleteRequest("employeeindex", "002");
+        DeleteResponse deleteResponse = client.delete(deleteRequest,
+                RequestOptions.DEFAULT);
+        // 削除した id を確認
+        System.out.println("response id: " + deleteResponse.getId());
+    }
+}
