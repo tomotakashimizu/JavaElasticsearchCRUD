91f29d9b1f5fb4d4ca3c86970b60eea86026b37d
tomotakashimizu <tomotaka.developer@gmail.com>
2021-09-17 18:59:30 +0900

    [add] Elasticsearch のデータを読み込むコードを作成
    GET _search
    {
      "query": {
        "match_all": {}
      }
    }
    と同じ結果

---
+++ src/main/java/elasticsearch/sample/read/ReadIndexData.java
@@ -0,0 +1,33 @@
+package elasticsearch.sample.read;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+public class ReadIndexData {
+
+    public static void main(String[] args) throws IOException {
+        // Elasticsearch に接続
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        System.out.println("searchResponse: " + searchResponse);
+
+        // クライアントを閉じる
+        client.close();
+    }
+}
